c2a7ba2168b901e957ce4fa4c57d0be77e241b79
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-12 15:42:49 +0000

    build based on 16d95e4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OMEinsum.jl-1","page":"Home","title":"OMEinsum.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [OMEinsum]","category":"page"},{"location":"#OMEinsum.greet-Tuple{}","page":"Home","title":"OMEinsum.greet","text":"greet() say hello to the world!\n\n\n\n\n\n","category":"method"}]
+}
