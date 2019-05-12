16d95e46db0556b51d34f4b96994ca343a3f2c1e
Andreas Peter <andreas.peter.ch@gmail.com>
2019-05-12 17:31:40 +0200

    Remove empty assets vector for Documenter.jl build to succeed

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,6 @@ makedocs(;
     repo="https://github.com/under-Peter/OMEinsum.jl/blob/{commit}{path}#L{line}",
     sitename="OMEinsum.jl",
     authors="Andreas Peter",
-    assets=[],
 )
 
 deploydocs(;
--- src/OMEinsum.jl
+++ src/OMEinsum.jl
@@ -1,5 +1,9 @@
 module OMEinsum
 
+@doc raw"
+   greet()
+say hello to the world!
+"
 greet() = print("Hello World!")
 
 end # module
