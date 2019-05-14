77f2ee249fc2a35aae14bc59943d135f6bb7a6ef
Andreas Peter <andreas.peter.ch@gmail.com>
2019-05-14 13:39:14 +0200

    Implement exponential-space einsum
    Implementation based on broadcasting * and summing over contracted 
    dimensions. Can't take traces or diagonals but allows contractions and 
    star-contractions.

--- src/OMEinsum.jl
+++ src/OMEinsum.jl
@@ -1,9 +1,66 @@
 module OMEinsum
+export einsum
+
+function einsum(cs, ts)
+    allins  = reduce(vcat, collect.(cs))
+    outinds = sort(filter(x -> count(==(x), allins) == 1, allins))
+    einsum(cs, ts, outinds)
+end
 
 @doc raw"
-   greet()
-say hello to the world!
+    einsum(cs, ts, out)
+return the tensor that results from contracting the tensors `ts` according
+to their indices `cs`, where twice-appearing indices are contracted.
+The result is permuted according to `out`.
+
+- `cs` - tuple of tuple of integers that label all indices of a tensor.
+       Indices that appear twice (in different tensors) are summed over
+
+- `ts` - tuple of tensors
+
+- `out` - tuple of integers that should correspond to remaining indices in `cs` after contractions.
+
+This implementation has space requirements that are exponential in the number of unique indices.
+
+# example
+```jldoctest; setup = :(using OMEinsum)
+julia> a = rand(2,2);
+
+julia> b = rand(2,2);
+
+julia> einsum(((1,2),(2,3)), (a, b), (1,3)) ≈ a * b
+true
+
+julia> einsum(((1,2),(2,3)), (a, b), (3,1)) ≈ permutedims(a * b, (2,1))
+true
+```
 "
-greet() = print("Hello World!")
+function einsum(contractions, tensors, outinds)
+    T = mapreduce(eltype, promote_type, tensors)
+    l = length(tensors)
+    allins = reduce(vcat, collect.(contractions))
+    uniqueallins = unique(allins)
+    ntensors = permuteandreshape.(Ref(uniqueallins), tensors, contractions)
+
+    ds = unique([i for i in allins if count(==(i), allins) > 1])
+    ds = map(i -> findfirst(==(i), uniqueallins), ds)
+
+    t = sum(broadcast(*, ntensors...), dims=ds)
+    tf = dropdims(t, dims = tuple(ds...))
+    x = [i for i in uniqueallins if i in outinds]
+    p = map(i -> findfirst(==(i),x), outinds)
+    tff = permutedims(tf,p)
+end
+
+function permuteandreshape(uniqueallins, t, c)
+    x = [i for i in uniqueallins if i in c]
+    p = map(i -> findfirst(==(i),x), c)
+    rs = map(uniqueallins) do i
+            j = findfirst(==(i), c)
+            isnothing(j) && return 1
+            return size(t,j)
+        end
+    return reshape(permutedims(t,p),rs...)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,31 @@ using Test
 
 @testset "OMEinsum.jl" begin
     # Write your own tests here.
+    a,b,c = randn(2,2), rand(2,2), rand(2,2)
+    v = rand(2)
+    t = randn(2,2,2,2)
+    @test einsum(((1,2),(2,3),(3,4)), (a,b,c)) ≈ a * b * c
+    @test einsum(((1,20),(20,3),(3,4)), (a,b,c)) ≈ a * b * c
+    @test einsum(((1,2),(2,3),(3,4)), (a,b,c), (4,1)) ≈ permutedims(a*b*c, (2,1))
+    @test einsum(((1,2),(2,)), (a,v)) ≈ a * v
+
+    @test_broken einsum(((1,1),), (a,))
+    @test einsum(((1,2),), (a,), (2,1)) ≈ permutedims(a,(2,1))
+    ta = zeros(size(t)[[1,2]]...)
+    for (i,j,k,l) in Iterators.product(1:2,1:2,1:2,1:2)
+        ta[i,l] += t[i,j,k,l] * a[j,k]
+    end
+    @test einsum(((1,2,3,4), (2,3)), (t,a)) ≈  ta
+
+    ta = zeros(size(t)[[1,2]]...)
+    for (i,j,k,l) in Iterators.product(1:2,1:2,1:2,1:2)
+        ta[i,l] += t[l,k,j,i] * a[j,k]
+    end
+    @test einsum(((4,3,2,1), (2,3)), (t,a),(1,4)) ≈  ta
+
+    aaa = zeros(2,2,2);
+    for (i,j,k,l) in Iterators.product(1:2,1:2,1:2,1:2)
+        aaa[j,k,l] += a[i,j] * a[i,k] * a[i,l]
+    end
+    @test aaa ≈ einsum(((1,2),(1,3),(1,4)), (a,a,a))
 end
