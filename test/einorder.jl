8ea4e0dd81a4c4213c2f8204484407c25d18f1c4
Andreas Peter <andreas.peter.ch@gmail.com>
2019-06-20 16:17:00 +0200

    Add unit-tests

--- test/einorder.jl
+++ test/einorder.jl
@@ -1,8 +1,8 @@
 using Test
 using OMEinsum
-using OMEinsum: edgesfrominds, operatorfromedge
+using OMEinsum: edgesfrominds, operatorfromedge, operatorsfromedges, supportinds
 using OMEinsum: TensorContract, Trace, StarContract, MixedStarContract, Diag,
-                MixedDiag, IndexReduction
+                MixedDiag, IndexReduction, Permutation, OuterProduct
 
 @testset "einorder" begin
     @testset "edges" begin
@@ -24,4 +24,22 @@ using OMEinsum: TensorContract, Trace, StarContract, MixedStarContract, Diag,
         @test operatorfromedge(3, ((1,2),(1,3),(1,1)), (1,2)) isa IndexReduction
     end
 
+    @testset "supportinds" begin
+        @test supportinds(1, ((1,2),(1,3),(1,4))) == (true, true, true)
+        @test supportinds(2, ((1,2),(1,3),(1,4))) == (true, false, false)
+        @test supportinds(3, ((1,2),(1,3),(1,4))) == (false, true, false)
+        @test supportinds(4, ((1,2),(1,3),(1,4))) == (false, false, true)
+    end
+
+    @testset "operators from edges" begin
+        @test operatorsfromedges(((1,2,3),(2,3,4)), [2,3], (1,4)) == (TensorContract((2,3)),)
+        @test operatorsfromedges(((1,2,3),(2,3,4)), [2,3], (4,1)) == (TensorContract((2,3)), Permutation((2,1)))
+        @test operatorsfromedges(((1,),(2,),(3,)), [], (1,2,3)) == (OuterProduct{3}(),)
+        @test operatorsfromedges(((1,1,2),(2,3)), [1,2], (3,)) == (Trace(1), TensorContract(2))
+        @test operatorsfromedges(((1,1,2),(2,3)), [2,1], (3,)) == (TensorContract(2), Trace(1))
+        @test operatorsfromedges(((1,1,2),(2,3)), [1,2], (1,3)) == (MixedDiag(1), TensorContract(2))
+        @test operatorsfromedges(((1,2),(2,3,4),(1,5,6),(3,5,7,8)), [1,2,3,5], (4,6,7,8)) ==
+            (TensorContract(1), TensorContract(2), TensorContract((3,5)), Permutation((2,1,3,4)))
+        @test operatorsfromedges(((1,2,3),), [], (1,3,2)) == (Permutation((1,3,2)),)
+    end
 end
