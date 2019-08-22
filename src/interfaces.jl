bf3a2d926536d13946328cb1ee2eea939da994c4
Andreas Peter <andreas.peter.ch@gmail.com>
2019-08-22 15:39:25 +0200

    Rename the fields of IndexSize

--- src/interfaces.jl
+++ src/interfaces.jl
@@ -49,25 +49,25 @@ function get_size_dict(ixs::NTuple{N, NTuple{M, T} where M}, xs::NTuple{X, Abstr
 end
 
 struct IndexSize{N,T}
-    k::NTuple{N,T}
-    v::NTuple{N,Int}
+    indices::NTuple{N,T}
+    sizes::NTuple{N,Int}
 end
 
 IndexSize(sizes::Pair...) = IndexSize(first.(sizes), last.(sizes))
-Base.:+(x::IndexSize, y::IndexSize) = IndexSize((x.k..., y.k...), (x.v..., y.v...))
+Base.:+(x::IndexSize, y::IndexSize) = IndexSize((x.indices..., y.indices...), (x.sizes..., y.sizes...))
 
 function getindexsize(ixs, xs)
-    k = TupleTools.flatten(ixs)
-    v = TupleTools.flatten(map(size,xs))
-    IndexSize(k,v)
+    indices = TupleTools.flatten(ixs)
+    sizes = TupleTools.flatten(map(size,xs))
+    IndexSize(indices,sizes)
 end
 
-Base.getindex(inds::IndexSize{N,T},i::T) where {N,T} = inds.v[findfirst(==(i), inds.k)]
+Base.getindex(inds::IndexSize{N,T},i::T) where {N,T} = inds.sizes[findfirst(==(i), inds.indices)]
 
 function check_dimensions(inds::IndexSize)
-    for (c,i) in enumerate(inds.k)
-        j = findnext(==(i), inds.k, c+1)
-        j != nothing && inds.v[c] != inds.v[j] && return throw(
+    for (c,i) in enumerate(inds.indices)
+        j = findnext(==(i), inds.indices, c+1)
+        j != nothing && inds.sizes[c] != inds.sizes[j] && return throw(
             DimensionMismatch("index $i has incosistent sizes"))
     end
     return true
