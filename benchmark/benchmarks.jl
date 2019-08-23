f6201a20a88d95334c2c2742c5b6f9f1c68277d8
Andreas Peter <andreas.peter.ch@gmail.com>
2019-08-23 15:50:52 +0200

    change benchmarks for gpu

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,7 +1,11 @@
 using BenchmarkTools
 using Random
-using OMEinsum
+using OMEinsum, CuArray
 
+using CUDAnative: device!
+error()
+device!(0)
+CuArrays.allowscalar(false)
 
 Random.seed!(0)
 
@@ -10,15 +14,20 @@ const SUITE = BenchmarkGroup()
 # Matrix multiplication
 SUITE["matmul"] = BenchmarkGroup()
 suite = SUITE["matmul"]
+
+function matmul(a,b)
+    CuArrays.@sync ein"ij,jk -> ik"(a,b)
+end
+￼
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
-    args = ["tiny"   => rand(T,2,2),
-    "small"  => rand(T,10,10),
-    "medium" => rand(T,10^2,10^2),
-    "large"  => rand(T, 10^3,10^3)
+    args = ["tiny"   => CuArray(rand(T,2,2)),
+    "small"  => CuArray(rand(T,10,10)),
+    "medium" => CuArray(rand(T,10^2,10^2)),
+    "large"  => CuArray(rand(T, 10^3,10^3))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ij,jk -> ik"($m,$m)
+        suite[string(T)][k] = @benchmarkable matmul($m,$m)
     end
 end
 
@@ -27,59 +36,72 @@ SUITE["batchmul"] = BenchmarkGroup()
 suite = SUITE["batchmul"]
 
 
+function batchmul(a,b)
+    CuArrays.@sync ein"ijk,jlk -> ilk"(a,b)
+end
+
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
-    args = ["tiny"   => rand(T,2,2,3),
-    "small"  => rand(T,10,10,3),
-    "medium" => rand(T,10^2,10^2,3),
-    "large"  => rand(T, 10^3,10^3,3)
+    args = ["tiny"   => CuArray(rand(T,2,2,3)),
+    "small"  => CuArray(rand(T,10,10,3)),
+    "medium" => CuArray(rand(T,10^2,10^2,3)),
+    "large"  => CuArray(rand(T, 10^3,10^3,3))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ijk,jlk -> ilk"($m,$m)
+        suite[string(T)][k] = @benchmarkable batchmul($m,$m)
     end
 end
 
+function mydot(a,b)
+    CuArrays.@sync ein"ijk,ijk -> "(a,b)
+end
 #inner - reduction to scalar
 SUITE["dot"] = BenchmarkGroup()
 suite = SUITE["dot"]
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
-    args = ["tiny"   => rand(T,fill(2,3)...)
-    "small"  => rand(T,fill(10,3)...)
-    "medium" => rand(T,fill(20,3)...)
-    "large"  => rand(T,fill(50,3)...)
-    "huge"   => rand(T,fill(10^2,3)...)]
+    args = ["tiny"   => CuArray(rand(T,fill(2,3)...))
+    "small"  => CuArray(rand(T,fill(10,3)...))
+    "medium" => CuArray(rand(T,fill(20,3)...))
+    "large"  => CuArray(rand(T,fill(50,3)...))
+    "huge"   => CuArray(rand(T,fill(10^2,3)...))]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ijk,ijk -> "($m, $m)
+        suite[string(T)][k] = @benchmarkable mydot($m, $m)
     end
 end
 
+function mytrace(a)
+    CuArrays.@sync ein"ii -> "(a)
+end
 #trace
 SUITE["trace"] = BenchmarkGroup()
 suite = SUITE["trace"]
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
-    args = ["tiny"   => rand(T,fill(2,2)...)
-    "small"  => rand(T,fill(10,2)...)
-    "medium" => rand(T,fill(10^2,2)...)
-    "large"  => rand(T,fill(10^3,2)...)]
+    args = ["tiny"   => CuArray(rand(T,fill(2,2)...))
+    "small"  => CuArray(rand(T,fill(10,2)...))
+    "medium" => CuArray(rand(T,fill(10^2,2)...))
+    "large"  => CuArray(rand(T,fill(10^3,2)...))]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ii -> "($m)
+        suite[string(T)][k] = @benchmarkable mytrace($m)
     end
 end
 
+function myptrace(a)
+    CuArrays.@sync ein"iij -> j"(a)
+end
 #partial trace
 SUITE["ptrace"] = BenchmarkGroup()
 suite = SUITE["ptrace"]
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
-    args = ["tiny"   => rand(T,fill(2,3)...)
-    "small"  => rand(T,fill(10,3)...)
-    "medium" => rand(T,fill(20,3)...)
-    "large"  => rand(T,fill(50,3)...)
-    "huge"   => rand(T,fill(10^2,3)...)]
+    args = ["tiny"   => CuArray(rand(T,fill(2,3)...))
+    "small"  => CuArray(rand(T,fill(10,3)...))
+    "medium" => CuArray(rand(T,fill(20,3)...))
+    "large"  => CuArray(rand(T,fill(50,3)...))
+    "huge"   => CuArray(rand(T,fill(10^2,3)...))]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"iij -> j"($m)
+        suite[string(T)][k] = @benchmarkable myptrace($m)
     end
 end
 
@@ -87,33 +109,41 @@ end
 SUITE["diag"] = BenchmarkGroup()
 suite = SUITE["diag"]
 
+function mydiag(a)
+    CuArrays.@sync ein"iij -> ij"(a)
+end
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(2,3)...)
-    "small"  => rand(T,fill(10,3)...)
-    "medium" => rand(T,fill(20,3)...)
-    "large" => rand(T,fill(30,3)...)
-    "huge" => rand(T,fill(100,3)...)
+    "tiny"   => CuArray(rand(T,fill(2,3)...))
+    "small"  => CuArray(rand(T,fill(10,3)...))
+    "medium" => CuArray(rand(T,fill(20,3)...))
+    "large" => CuArray(rand(T,fill(30,3)...))
+    "huge" => CuArray(rand(T,fill(100,3)...))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ijj -> ij"($m)
+        suite[string(T)][k] = @benchmarkable mydiag($m)
     end
 end
 
 #permutation
 SUITE["perm"] = BenchmarkGroup()
 suite = SUITE["perm"]
+
+function myperm(a)
+    CuArrays.@sync ein"ijkl -> ljki"(a)
+end
+
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(1,4)...)
-    "small"  => rand(T,fill(2,4)...)
-    "medium" => rand(T,fill(10,4)...)
-    "large"  => rand(T,fill(30,4)...)
+    "tiny"   => CuArray(rand(T,fill(1,4)...))
+    "small"  => CuArray(rand(T,fill(2,4)...))
+    "medium" => CuArray(rand(T,fill(10,4)...))
+    "large"  => CuArray(rand(T,fill(30,4)...))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ijkl -> ljki"($m)
+        suite[string(T)][k] = @benchmarkable myperm($m)
     end
 end
 
@@ -121,17 +151,20 @@ end
 SUITE["tcontract"] = BenchmarkGroup()
 suite = SUITE["tcontract"]
 
+function tcontract(a,b)
+    CuArrays.@sync ein"ijk, jlk -> il"(a,b)
+end
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(1,3)...)
-    "small"  => rand(T,fill(2,3)...)
-    "medium" => rand(T,fill(10,3)...)
-    "large"  => rand(T,fill(30,3)...)
+    "tiny"   => CuArray(rand(T,fill(1,3)...))
+    "small"  => CuArray(rand(T,fill(2,3)...))
+    "medium" => CuArray(rand(T,fill(10,3)...))
+    "large"  => CuArray(rand(T,fill(30,3)...))
     ]
 
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ijk, jlk -> il"($m,$m)
+        suite[string(T)][k] = @benchmarkable tcontract($m,$m)
     end
 end
 
@@ -139,16 +172,19 @@ end
 SUITE["star"] = BenchmarkGroup()
 suite = SUITE["star"]
 
+function mystar(a,b,c)
+    CuArrays.@sync ein"ij,ik,il -> jkl"(a,b,c)
+end
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(2,2)...)
-    "small"  => rand(T,fill(10,2)...)
-    "medium" => rand(T,fill(30,2)...)
-    "large"  => rand(T,fill(100,2)...)
+    "tiny"   => CuArray(rand(T,fill(2,2)...))
+    "small"  => CuArray(rand(T,fill(10,2)...))
+    "medium" => CuArray(rand(T,fill(30,2)...))
+    "large"  => CuArray(rand(T,fill(100,2)...))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ij,ik,il -> jkl"($m,$m,$m)
+        suite[string(T)][k] = @benchmarkable mystar($m,$m,$m)
     end
 end
 
@@ -156,47 +192,59 @@ end
 SUITE["starandcontract"] = BenchmarkGroup()
 suite = SUITE["starandcontract"]
 
+function mystarandcontract(a,b,c)
+    CuArrays.@sync ein"ij,ik,ik -> j"(a,b,c)
+end
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(2,2)...)
-    "small"  => rand(T,fill(10,2)...)
-    "medium" => rand(T,fill(30,2)...)
-    "large"  => rand(T,fill(100,2)...)
+    "tiny"   => CuArray(rand(T,fill(2,2)...))
+    "small"  => CuArray(rand(T,fill(10,2)...))
+    "medium" => CuArray(rand(T,fill(30,2)...))
+    "large"  => CuArray(rand(T,fill(100,2)...))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ij,ik,ik -> j"($m,$m,$m)
+        suite[string(T)][k] = @benchmarkable mystarandcontract($m,$m,$m)
     end
 end
 
 # index-sum
 SUITE["indexsum"] = BenchmarkGroup()
 suite = SUITE["indexsum"]
+
+function indexsum(a)
+    CuArrays.@sync ein"ijk -> ik"(a)
+end
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(2,3)...)
-    "small"  => rand(T,fill(10,3)...)
-    "medium" => rand(T,fill(30,3)...)
-    "large"  => rand(T,fill(100,3)...)
+    "tiny"   => CuArray(rand(T,fill(2,3)...))
+    "small"  => CuArray(rand(T,fill(10,3)...))
+    "medium" => CuArray(rand(T,fill(30,3)...))
+    "large"  => CuArray(rand(T,fill(100,3)...))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ijk -> ik"($m)
+        suite[string(T)][k] = @benchmarkable indexsum($m)
     end
 end
+
 # Hadamard
 SUITE["hadamard"] = BenchmarkGroup()
 suite = SUITE["hadamard"]
+
+function hadamard(a,b)
+    CuArrays.@sync ein"ijk,ijk -> ijk"(a,b)
+end
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(2,3)...)
-    "small"  => rand(T,fill(10,3)...)
-    "medium" => rand(T,fill(30,3)...)
-    "large"  => rand(T,fill(100,3)...)
+    "tiny"   => CuArray(rand(T,fill(2,3)...))
+    "small"  => CuArray(rand(T,fill(10,3)...))
+    "medium" => CuArray(rand(T,fill(30,3)...))
+    "large"  => CuArray(rand(T,fill(100,3)...))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ijk,ijk -> ijk"($m,$m)
+        suite[string(T)][k] = @benchmarkable hadamard($m,$m)
     end
 end
 
@@ -204,15 +252,18 @@ end
 SUITE["outer"] = BenchmarkGroup()
 suite = SUITE["outer"]
 
+function myouter(a,b)
+    CuArrays.@sync ein"ij,kl -> ijkl"(a,b)
+end
 for T in (Float32, Float64, ComplexF32, ComplexF64)
     suite[string(T)] = BenchmarkGroup()
     args =  [
-    "tiny"   => rand(T,fill(2,2)...)
-    "small"  => rand(T,fill(10,2)...)
-    "medium" => rand(T,fill(50,2)...)
-    "large"  => rand(T,fill(100,2)...)
+    "tiny"   => CuArray(rand(T,fill(2,2)...))
+    "small"  => CuArray(rand(T,fill(10,2)...))
+    "medium" => CuArray(rand(T,fill(50,2)...))
+    "large"  => CuArray(rand(T,fill(100,2)...))
     ]
     for (k,m) in args
-        suite[string(T)][k] = @benchmarkable ein"ij,kl -> ijkl"($m,$m)
+        suite[string(T)][k] = @benchmarkable myouter($m,$m)
     end
 end
