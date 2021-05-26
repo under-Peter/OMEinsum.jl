32fc7478e99b542f462c1c0323dff8c117ea0e75
GiggleLiu <cacate0129@gmail.com>
2021-05-26 12:05:36 -0400

    rm asarray type restriction

--- src/utils.jl
+++ src/utils.jl
@@ -1,13 +1,13 @@
 export asarray
 
 """
-    asarray(x::Number[, parent::AbstractArray]) -> AbstactArray
+    asarray(x[, parent::AbstractArray]) -> AbstactArray
 
 Return a 0-dimensional array with item `x`, otherwise, do nothing.
 If a `parent` is supplied, it will try to match the parent array type.
 """
-asarray(x::Number) = fill(x, ())
-asarray(x::Number, arr::Array) = fill(x, ())
+asarray(x) = fill(x, ())
+asarray(x, arr::Array) = fill(x, ())
 asarray(x::AbstractArray, args...) = x
 
 """
