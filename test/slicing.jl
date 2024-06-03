dd75c4423ad1aa45a6f90d9fea808998cace8dd1
nzy <nzy@nzy-3.local>
2024-06-03 15:30:41 +0800

    update

--- test/slicing.jl
+++ test/slicing.jl
@@ -31,7 +31,7 @@ end
 
 @testset "replace" begin
     code = ein"(ij, jk), kl->il"
-    se = optimize_code(code, uniformsize(code, 2), TreeSA())
+    se = optimize_code(code, uniformsize(code, 2), TreeSA(;niters=0))
     se2 = replace(se, 'i'=>'a', 'j'=>'b', 'k'=>'c', 'l'=>'d')
     @test labeltype(se2) == Char
     @test se2 == SlicedEinsum(Char[], ein"(ab, bc), cd->ad")
